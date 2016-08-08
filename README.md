74ee4d3921b2eb031ff331f9c43536ac27d50546
Zsupos Gábor <g.zsupos@mito.hu>
2016-08-08 16:20:24 +0200

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# grid
+# [Grid]
+
+Flex based CSS grid
+
+* Chrome
+* Edge
+* Firefox
+* Internet Explorer 10+
+* Opera
+* Safari 9.1+
+
+### usage
+
+### development
+
+Install the npm packages described in the `package.json` and verify that it works:
+
+```bash
+git clone https://github.com/gzsupos/grid.git
+npm install
+gulp
+```
+#
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
-var cssmin = require('gulp-cssmin');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var merge = require('merge-stream');
 
-var scssPath = './src/scss/**/*.scss';
+var demoPath = './demo/scss/**/*.scss';
+var gridpath = './src/scss/**/*.scss';
 
-gulp.task('sass', function() {
-    var normal = gulp.src(scssPath)
+gulp.task('demo', function() {
+    return gulp.src(demoPath)
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./demo/css'));
+});
+
+gulp.task('grid', function() {
+    var normal = gulp.src(gridpath)
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 3 versions'],
             cascade: false
@@ -19,14 +33,15 @@ gulp.task('sass', function() {
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/css/'));
 
-    var compressed = gulp.src(scssPath)
+    var compressed = gulp.src(gridpath)
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 3 versions'],
             cascade: false
         }))
-        .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/css/'));
@@ -35,9 +50,10 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(scssPath, ['sass']);
+    gulp.watch(demoPath, ['demo']);
+    gulp.watch(gridpath, ['grid']);
 });
 
-gulp.task('default', ['sass', 'watch'], function(done) {
+gulp.task('default', ['demo', 'grid', 'watch'], function(done) {
     done();
 });
